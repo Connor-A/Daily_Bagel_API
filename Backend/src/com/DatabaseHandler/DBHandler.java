b4f48c7fb70d3a666e4f913268db9b3c34a1182d
Connor Abla <Connor.Abla@cgi.com>
2019-07-29 11:50:05 -0400

    Initialization
    Inital filestructure

---
+++ Backend/src/com/DatabaseHandler/DBHandler.java
@@ -0,0 +1,30 @@
+package com.DatabaseHandler;
+
+import java.io.File;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+ 
+public class DBHandler
+{
+    private static final SessionFactory sessionFactory;
+    
+    static {
+    	try {
+            sessionFactory =  new AnnotationConfiguration().configure().buildSessionFactory();
+ 
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+ 
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
