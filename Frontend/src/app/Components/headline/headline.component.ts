5b40aff5078903b36c597eb3e81e1176839b55cb
Connor Abla <Connor.Abla@cgi.com>
2019-07-31 14:30:07 -0400

    headline component + service

---
+++ Frontend/src/app/Components/headline/headline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headline',
+  templateUrl: './headline.component.html',
+  styleUrls: ['./headline.component.css']
+})
+export class HeadlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FeaturedContentComponent } from './components/featured-content/featured-content.component';
 import { StoryComponent } from './objects/story/story.component';
+import { HeadlineComponent } from './components/headline/headline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeaturedContentComponent,
-    StoryComponent
+    StoryComponent,
+    HeadlineComponent
   ],
   imports: [
     BrowserModule
