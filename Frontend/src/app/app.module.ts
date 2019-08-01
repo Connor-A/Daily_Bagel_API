f0f1c3b9f19dd235e46e2ec4f393e59422a64f82
Jonah Johnson <jonahj95@gmail.com>
2019-08-01 09:24:14 -0400

    Added Material Dependencies

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { FeaturedContentComponent } from './components/featured-content/featured-content.component';
-import { StoryComponent } from './objects/story/story.component';
+//import { StoryComponent } from './objects/story/story.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeaturedContentComponent,
-    StoryComponent
+//    StoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
