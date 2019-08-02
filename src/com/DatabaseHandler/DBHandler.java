378fb87fe14b200aa24832320e9db5a4ab2c7865
Connor Abla <Connor.Abla@cgi.com>
2019-08-02 13:29:23 -0400

    Adding new functions

--- src/com/DatabaseHandler/DBHandler.java
+++ src/com/DatabaseHandler/DBHandler.java
@@ -12,42 +12,56 @@ import com.dailybagel.user.resources.User;
 
 public class DBHandler
 {
-    private static final SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
     
     static {
-    	try {
-    		Properties prop= new Properties();
-            prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-            prop.setProperty("hibernate.connection.url", "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_d80dc7ab129a7ce?reconnect=true");
-            prop.setProperty("hibernate.connection.username", "baaa95d23db726");
-            prop.setProperty("hibernate.connection.password", "de7ce257");
-            prop.setProperty("dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-            prop.setProperty("connection.autoReconnect", "true");
-            prop.setProperty("connection.autoReconnectForPools", "true");
-            prop.setProperty("connection.is-connection-validation-required","true");
-            prop.setProperty("hibernate.c3p0.min_size", "5");
-            prop.setProperty("hibernate.c3p0.max_size", "50");
-            prop.setProperty("hibernate.c3p0.timeout", "1800");
-            prop.setProperty("hibernate.c3p0.max_statements","5");
-            prop.setProperty("hibernate.c3p0.idle_test_period","100");
-            sessionFactory = new AnnotationConfiguration()
-            				.addPackage("com.dailybagel.user.resources")
-            				.addPackage("com.dailybagel.article.resources")
-            				.addAnnotatedClass(User.class)
-            				.addAnnotatedClass(Article.class)
-                            .addProperties(prop)                  
-                            .buildSessionFactory();
-            
-        } catch (Throwable ex) {
+    	try { resetConnection(); } 
+    	
+    	catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
  
+    public static SessionFactory resetConnection() {
+		Properties prop= getProps();
+         return sessionFactory = new AnnotationConfiguration()
+        				.addPackage("com.dailybagel.user.resources")
+        				.addPackage("com.dailybagel.article.resources")
+        				.addAnnotatedClass(User.class)
+        				.addAnnotatedClass(Article.class)
+                        .addProperties(prop)                  
+                        .buildSessionFactory();
+
+    }
+    
+    private static Properties getProps() {
+    	Properties prop = new Properties();
+    	prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        prop.setProperty("hibernate.connection.url", "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_d80dc7ab129a7ce?reconnect=true");
+        prop.setProperty("hibernate.connection.username", "baaa95d23db726");
+        prop.setProperty("hibernate.connection.password", "de7ce257");
+        prop.setProperty("dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
+        prop.setProperty("connection.autoReconnect", "true");
+        prop.setProperty("connection.autoReconnectForPools", "true");
+        prop.setProperty("connection.is-connection-validation-required","true");
+        prop.setProperty("hibernate.c3p0.min_size", "5");
+        prop.setProperty("hibernate.c3p0.max_size", "50");
+        prop.setProperty("hibernate.c3p0.timeout", "1800");
+        //prop.setProperty("hibernate.c3p0.max_statements","5");
+        prop.setProperty("hibernate.c3p0.idle_test_period","100");
+        return prop;
+    }
+    
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
- 
+    public static void testSession() {
+    	if(sessionFactory.isClosed()) {
+    		resetConnection();
+    	}
+    }
+    
     public static void shutdown() {
         getSessionFactory().close();
     }
