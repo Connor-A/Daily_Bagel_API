f20389b85b7450518897674151a51d38ec977d54
Connor Abla <Connor.Abla@cgi.com>
2019-08-05 09:15:13 -0400

    Trying new config on remote

--- src/com/DatabaseHandler/DBHandler.java
+++ src/com/DatabaseHandler/DBHandler.java
@@ -15,11 +15,11 @@ public class DBHandler
     static {
     	try {
     		Properties prop= new Properties();
-            prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            /*prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
             prop.setProperty("hibernate.connection.url", "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_d80dc7ab129a7ce?reconnect=true");
             prop.setProperty("hibernate.connection.username", "baaa95d23db726");
             prop.setProperty("hibernate.connection.password", "de7ce257");
-            prop.setProperty("dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
+            prop.setProperty("dialect", "org.hibernate.dialect.MySQLInnoDBDialect");*/
             //prop.setProperty("hibernate.c3p0.min_size", "5");
             //prop.setProperty("hibernate.c3p0.max_size", "200");
             //prop.setProperty("hibernate.c3p0.timeout", "9010");
@@ -30,7 +30,7 @@ public class DBHandler
             				.addPackage("com.dailybagel.article.resources")
             				.addAnnotatedClass(User.class)
             				.addAnnotatedClass(Article.class)
-                            .addProperties(prop)                  
+                            //.addProperties(prop)                  
                             .buildSessionFactory();
             
         } catch (Throwable ex) {
