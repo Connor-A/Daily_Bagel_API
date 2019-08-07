2c1b586cffa2b12ee7f750aefb2af827209ec6c2
Connor Abla <Connor.Abla@cgi.com>
2019-08-07 10:56:57 -0400

    Update AuthService.java

--- src/main/java/com/dailybagel/auth/AuthService.java
+++ src/main/java/com/dailybagel/auth/AuthService.java
@@ -29,7 +29,7 @@ public class AuthService {
 		
 		User u = new User();
 		
-		Query q = session.createQuery("from User u where u.email = " + email + "and u password = " + password);
+		Query q = session.createQuery("from User u where u.email = " + email + "and u.password = " + password);
 		SecureRandom random = new SecureRandom();
 		byte bytes[] = new byte[20];
 		random.nextBytes(bytes);
