d19a40ac09082aeb3a33814509838e43dbe27b25
Jonah Johnson <jonahj95@gmail.com>
2019-08-07 14:35:07 -0400

    Added server status checking for UserService

--- src/main/java/com/dailybagel/DatabaseHandler/DBHandler.java
+++ src/main/java/com/dailybagel/DatabaseHandler/DBHandler.java
@@ -1,7 +1,11 @@
 package com.dailybagel.DatabaseHandler;
 
 import java.io.File;
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
@@ -22,6 +26,24 @@ public class DBHandler
  
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+    
+    public static boolean testDBConnection() throws InterruptedException, IOException {
+    	Session session = sessionFactory.openSession();
+    	int numTries = 10;
+    	
+    	for (int i = 0; i < numTries; i++) {
+    		try {
+    			session.createSQLQuery("SELECT 1").uniqueResult();
+    		} catch (Throwable e) { 
+    			Thread.sleep(1000);
+    			continue;
+    		}
+    		session.close();
+    		return true;
+    	}
+    	session.close();
+    	return false;
     }
  
     public static void shutdown() {
